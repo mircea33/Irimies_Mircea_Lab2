8df3106c59b8164f0968e4ca7adec39d8c6beac6
mircea33 <72373515+mircea33@users.noreply.github.com>
2021-10-13 19:47:54 +0300

    Add project files.

---
+++ Irimies_Mircea_Lab2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Irimies_Mircea_Lab2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
